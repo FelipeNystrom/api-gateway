91a52f9d5fb435c3c4eaefd42406a86480620d8f
Felipe Nystrom <felipe@pelif.se>
2019-05-16 08:57:32 +0200

    setup token auth start

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const serverSetup = require('./serverConfig');
 const serviceDiscovery = require('./_services-setup');
+const passport = require('passport');
+const passportConfig = require('./_token-auth');
 const server = express();
 const _port = process.env.PORT || 3001;
 
 serverSetup(server);
-
+passportConfig(passport);
 serviceDiscovery(server);
-
 server.listen(_port, () => {
   console.log('server is running on ' + _port);
 });
